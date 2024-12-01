005d95f5d99f0f2c60ba373b62e56292244d648d
ryanmio <ryan@mioduski.us>
2024-11-30 20:25:17 -0500

    fix: exclude cometary ice harvester from resource depletion status

--- js/game.js
+++ js/game.js
@@ -2702,8 +2702,8 @@ function createAccordionDevice(device, container) {
     let status = 'inactive';
     let statusText = 'Inactive';
 
-    // Check if this is a device that can be depleted (not a rover)
-    const isAutomationDevice = !['planting-rovers', 'harvesting-rovers', 'prospecting-rovers'].includes(device.id);
+    // Check if this is a device that can be depleted (not a rover or cometary harvester)
+    const isAutomationDevice = !['planting-rovers', 'harvesting-rovers', 'prospecting-rovers', 'cometary-ice-harvester'].includes(device.id);
     
     // First check if resources are depleted for automation devices
     if (isAutomationDevice && areResourcesDepleted) {
